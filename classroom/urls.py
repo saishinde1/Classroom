3da5e156b02deee06640b192d783a3d801adb342
DivyaM310 <mulikdivya1@gmail.com>
2024-04-23 12:27:08 +0530

    second

--- classroom/urls.py
+++ classroom/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .import views,hod_Views,staff_views,student_views
+from .import teacher_Views, views,hod_Views,student_Views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.divya,name='base'),
-    path('',views.login,name='login'),
+    path('',views.LOGIN,name='login'),
     path('dologin',views.dologin,name='dologin'),
     path('dologout',views.dologout,name='logout'),
     path('profile/', views.PROFILE,name='profile'),
@@ -34,6 +34,10 @@ urlpatterns = [
     path('Hod/Student/Update',hod_Views.UPDATE_STUDENT,name='update_student'),
     path('Hod/Student/Delete/<str:admin>',hod_Views.DELETE_STUDENT,name='delete_student'),
     
+    path('Hod/Student/Feedback',hod_Views.STUDENT_FEEDBACK,name='get_student_feedback'),
+    path('Hod/Student/Feedback/save',hod_Views.STUDENT_FEEDBACK_SAVE,name='student_feedback_reply_save'),
+
+
     path('Hod/Course/Add',hod_Views.ADD_COURSE,name='add_course'),
     path('Hod/Course/View',hod_Views.VIEW_COURSE,name='view_course'),
     path('Hod/Course/Edit/<str:id>',hod_Views.EDIT_COURSE,name='edit_course'),
@@ -45,5 +49,61 @@ urlpatterns = [
     path('Hod/Teacher/Edit/<str:id>', hod_Views.EDIT_TEACHER,name='edit_teacher'),
     path('Hod/Teacher/Update', hod_Views.UPDATE_TEACHER,name='update_teacher'),
     path('Hod/Teacher/Delete/<str:admin>',hod_Views.DELETE_TEACHER,name='delete_teacher'),
+
+    path('Hod/Subject/Add', hod_Views.ADD_SUBJECT, name='add_subject'),
+    path('Hod/Subject/View', hod_Views.VIEW_SUBJECT,name='view_subject'),
+    path('Hod/Subject/Edit/<str:id>', hod_Views.EDIT_SUBJECT,name='edit_subject'),
+    path('Hod/Subject/Update', hod_Views.UPDATE_SUBJECT,name='update_subject'),
+    path('Hod/Subject/Delete/<str:id>',hod_Views.DELETE_SUBJECT,name='delete_subject'),
+    
+   
+    path('Hod/Session/Add', hod_Views.ADD_SESSION, name='add_session'),
+    path('Hod/Session/View', hod_Views.VIEW_SESSION,name='view_session'),
+    path('Hod/Session/Edit/<str:id>', hod_Views.EDIT_SESSION,name='edit_session'),
+    path('Hod/Session/Update', hod_Views.UPDATE_SESSION,name='update_session'),
+    path('Hod/Session/Delete/<str:id>',hod_Views.DELETE_SESSION,name='delete_session'),
+    
+    path('Hod/Teacher/Leave_view',hod_Views.TEACHER_LEAVE_VIEW,name='teacher_leave_view'),
+    path('Hod/Teacher/Approve_leave/<str:id>',hod_Views.TEACHER_APPROVE_LEAVE,name='teacher_approve_leave'),
+    path('Hod/Teacher/Disapprove_leave/<str:id>',hod_Views.TEACHER_DISAPPROVE_LEAVE,name='teacher_disapprove_leave'),
+
+    path('Hod/Student/Leave_view',hod_Views.STUDENT_LEAVE_VIEW,name='student_leave_view'),
+    path('Hod/Student/Approve_leave/<str:id>',hod_Views.STUDENT_APPROVE_LEAVE,name='student_approve_leave'),
+    path('Hod/Student/Disapprove_leave/<str:id>',hod_Views.STUDENT_DISAPPROVE_LEAVE,name='student_disapprove_leave'),
+
+    path('Hod/View_attendace',hod_Views.HOD_VIEW_ATTENDANCE,name='hod_view_attendance'),
+
+    
+   
+    #this is for teacher
+
+    path('Teacher/home',teacher_Views.HOME,name='teacher_home'),
+    path('Teacher/student/add',teacher_Views.teach_ADD_STUDENT,name='teach_add_student'),
+    path('Teacher/Student/View',teacher_Views.teach_VIEW_STUDENT,name='teach_view_student'),
+    path('Teacher/Student/Edit/<str:id>',teacher_Views.teach_EDIT_STUDENT,name='teach_edit_student'),
+    path('Teacher/Student/Update',teacher_Views.teach_UPDATE_STUDENT,name='teach_update_student'),
+    path('Teacher/Student/Delete/<str:admin>',teacher_Views.teach_DELETE_STUDENT,name='teach_delete_student'),
+
+    path('Teacher/Apply_leave',teacher_Views.TEACHER_APPLY_LEAVE,name='teacher_apply_leave'),
+    path('Teacher/Apply_leave_save',teacher_Views.TEACHER_APPLY_LEAVE_SAVE,name='teacher_apply_leave_save'),
+
+    path('Teacher/Take_attendace',teacher_Views.TEACHER_TAKE_ATTENDANCE,name='teacher_take_attendance'),
+    path('Teacher/Save_attendace',teacher_Views.TEACHER_SAVE_ATTENDANCE,name='teacher_save_attendance'),
+    path('Teacher/View_attendace',teacher_Views.TEACHER_VIEW_ATTENDANCE,name='teacher_view_attendance'),
+
+
+    #this is for Student
+    path('Student/home',student_Views.HOME,name='student_home'),
+    
+    path('Student/feedback',student_Views.STUDENT_FEEDBACK,name='student_feedback'),
+    path('Student/feedback/save',student_Views.STUDENT_FEEDBACK_SAVE,name='student_feedback_save'),
+
+    path('Student/Apply_for_leave',student_Views.STUDENT_LEAVE,name='student_leave'),
+    path('Student/leave_save',student_Views.STUDENT_LEAVE_SAVE,name='student_leave_save'),
+    
+    path('Student/View_attendace',student_Views.STUDENT_VIEW_ATTENDANCE,name='student_view_attendance'),
+
+   
+
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- students/models.py
+++ students/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     USER=(
         (1,'HOD'),
-        (2,'STAFF'),
+        (2,'TEACHER'),
         (3,'STUDENT')
     )
     user_type=models.CharField(choices=USER, max_length=50,default=1)
@@ -49,3 +49,64 @@ class Teacher(models.Model):
     def __str__(self):
         return self.admin.username
     
+class Subject(models.Model):
+    name=models.CharField(max_length=100)
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    teacher=models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+
+class Teacher_leave(models.Model):
+    teacher_id=models.ForeignKey(Teacher,on_delete=models.CASCADE)
+    date=models.CharField(max_length=100)
+    message=models.TextField()
+    status=models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.teacher_id.admin.first_name + self.teacher_id.admin.last_name
+    
+class Student_Feedback(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField(null=True, default="")
+    status=models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+
+    def __str__(self):
+        return self.student_id.admin.first_name + " " + self.student_id.admin.last_name
+    
+class Student_Leave(models.Model):
+    student_id=models.ForeignKey(Student,on_delete=models.CASCADE)
+    date=models.CharField(max_length=100)
+    message=models.TextField()
+    status=models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.student_id.admin.first_name + self.student_id.admin.last_name
+
+class Attendance(models.Model):
+    subject_id=models.ForeignKey(Subject,on_delete=models.DO_NOTHING)
+    attendance_date=models.DateField()
+    session_year_id=models.ForeignKey(Session_Year,on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject_id.name
+    
+class Attendance_Report(models.Model):
+    student_id=models.ForeignKey(Student,on_delete=models.DO_NOTHING)
+    attendance_id=models.ForeignKey(Attendance,on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.student_id.admin.first_name
+
+
